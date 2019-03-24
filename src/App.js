4680bd0e9529c15c8f581fadeec1e8d2f3e5445d
Pieter Wolmarans <pieter@pixelume.com>
2019-03-24 20:44:37 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
-import Hangman from './Containers/Hangman/Hangman';
+import React, { Component } from "react";
+import Hangman from "./Containers/Hangman/Hangman";
 // import logo from './logo.svg';
 // import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <Hangman/>
-    );
-  }
+    render() {
+        return <Hangman />;
+    }
 }
 
 export default App;
