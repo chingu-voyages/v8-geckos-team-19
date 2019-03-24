81c7245e1ebf6c86d9a616dad1a87801c8f4a152
dianastanciu <dianastanciu20@gmail.com>
2019-03-24 16:32:35 +0200

    #6 change layoutName

--- src/App.js
+++ src/App.js
@@ -20,8 +20,9 @@ class App extends Component {
             this.onChange(input)}
           onKeyPress={button =>
             this.onKeyPress(button)}
+          layoutName={'letters'}
           layout={{
-            'default': [
+            'letters': [
               'q w e r t y u i o p',
               'a s d f g h j k l',
               'z x c v b n m'
