9115efa4f0ed2b010a0634c69e5ed3094c863a65
dianastanciu <dianastanciu20@gmail.com>
2019-04-02 00:33:47 +0300

    #6 style to keyboard buttons

--- src/Components/Hangman/Keyboard.js
+++ src/Components/Hangman/Keyboard.js
@@ -1,24 +1,35 @@
 import React, {Component} from "react";
+import Button from '../../Shared/UI/Button';
+
+let currentGuess;
 
 export default class extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            clicked: false,
+        }
         this.letterHandler = this.letterHandler.bind(this);
     }
 
     letterHandler = e => {
-        console.log(e.target.getAttribute('data-letter')) 
+        currentGuess = e.target.getAttribute('data-letter');
+        console.log('current guess is: ' + currentGuess);
     };
 
     render() {
         const alphabet = [
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q','R','S',
-            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+            'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+            'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+            'Z', 'X', 'C', 'V', 'B', 'N', 'M',
         ];
 
         const keyboard = alphabet.map((letter, index) => 
-            <button data-letter={letter} key={index} onClick={this.letterHandler}>{letter}</button>
+            <Button
+                data-letter={letter} 
+                key={index} 
+                onClick={this.letterHandler}
+            >{letter}</Button>
         );
 
         return (
