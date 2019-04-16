dc33658cf368fb0d8b8fad266f3a223b3b59bc8b
Laurent Mbuyu <laurent.mb19@gmail.com>
2019-04-17 01:40:14 +0200

    Simon design part 1

--- src/Containers/Simon/Simon.js
+++ src/Containers/Simon/Simon.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import './Simon.css';
 
 // This is the same css as for the Individual games containers. Should be shared - under layout component or something
-const GameWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    box-sizing: border-box;
-`
+// const GameWrapper = styled.div`
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     width: 100%;
+//     box-sizing: border-box;
+// `
 
 class Simon extends React.Component {
 
     render() {
-        console.log("Hey Laurent");
         return(
-            'Simon Simon Simon Simon Simon Simon Simon Simon Simon Simon Simon Simon Simon Simon'
+          <div className="grid-container">
+            <div className="grid-item"><button>Green</button></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"><button>Red</button></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"><div className="centerButton"> Simon </div></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"><button>Yellow</button></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"><button>Blue</button></div>
+          </div>
         );
     }
 }
