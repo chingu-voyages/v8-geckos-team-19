74a144b837163733a29830c23af28245472f1af6
Laurent Mbuyu <laurent.mb19@gmail.com>
2019-04-17 22:34:46 +0200

    Added visual light on click

--- src/Containers/Simon/Simon.js
+++ src/Containers/Simon/Simon.js
@@ -14,18 +14,83 @@ import './Simon.css';
 
 class Simon extends React.Component {
 
+    constructor(props)
+    {
+      super(props);
+
+      this.state = {
+        greenColor: 'grey',
+        redColor: 'grey',
+        yellowColor: 'grey',
+        blueColor: 'grey'
+      }
+
+      this.changeGreenColor = this.changeGreenColor.bind(this);
+      this.changeRedColor = this.changeRedColor.bind(this);
+      this.changeYellowColor = this.changeYellowColor.bind(this);
+      this.changeBlueColor = this.changeBlueColor.bind(this);
+
+      this.reset = this.reset.bind(this);
+
+      this.addToGame = this.addToGame.bind(this);
+    }
+
+    addToGame(){
+    }
+
+    reset(){
+      this.setState({
+        greenColor: 'grey',
+        redColor: 'grey',
+        yellowColor: 'grey',
+        blueColor: 'grey'
+      })
+    }
+
+    changeGreenColor(){
+      this.setState({
+        greenColor: 'green'
+      })
+
+      setTimeout(this.reset, 1000);
+    }
+
+    changeRedColor(){
+      this.setState({
+        redColor: 'red'
+      })
+
+      setTimeout(this.reset, 1000);
+    }
+
+    changeYellowColor(){
+      this.setState({
+        yellowColor: 'yellow'
+      })
+
+      setTimeout(this.reset, 1000);
+    }
+
+    changeBlueColor(){
+      this.setState({
+        blueColor: 'blue'
+      })
+
+      setTimeout(this.reset, 1000);
+    }
+
     render() {
         return(
           <div className="grid-container">
-            <div className="grid-item"><button>Green</button></div>
+            <div className="grid-item"><button id="greenButton" class="simonButton" onClick={this.changeGreenColor} style={{backgroundColor: this.state.greenColor}}>Green</button></div>
             <div className="grid-item"></div>
-            <div className="grid-item"><button>Red</button></div>
+            <div className="grid-item"><button id="redButton" class="simonButton" onClick={this.changeRedColor}  style={{backgroundColor: this.state.redColor}}>Red</button></div>
             <div className="grid-item"></div>
             <div className="grid-item"><div className="centerButton"> Simon </div></div>
             <div className="grid-item"></div>
-            <div className="grid-item"><button>Yellow</button></div>
+            <div className="grid-item"><button id="yellowButton" class="simonButton" onClick={this.changeYellowColor}  style={{backgroundColor: this.state.yellowColor}}>Yellow</button></div>
             <div className="grid-item"></div>
-            <div className="grid-item"><button>Blue</button></div>
+            <div className="grid-item"><button id="blueButton" class="simonButton" onClick={this.changeBlueColor}  style={{backgroundColor: this.state.blueColor}}>Blue</button></div>
           </div>
         );
     }
